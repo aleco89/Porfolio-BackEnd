d284ac41299f283877eea92621fc36226851ecc9
Ana <ana_leconte@hotmail.com>
2022-07-31 20:24:05 -0300

    commit final

--- backend/src/main/java/com/argprograma/backend/controller/ExperienciaController.java
+++ backend/src/main/java/com/argprograma/backend/controller/ExperienciaController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="http://localhost:4200")
+@CrossOrigin(origins={"http://localhost:4200","https://argprograma-c17ae.web.app"})
 @RequestMapping("experiencia")
 public class ExperienciaController {
     @Autowired
